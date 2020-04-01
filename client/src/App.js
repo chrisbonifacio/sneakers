5090308fc2dc31034c1e1342f66b55113eee95a1
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-03-31 20:20:54 -0400

    import and create route for SneakerPage

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,29 @@ import "./App.scss"
 import Navbar from "./components/Navigation/Navbar"
 import Cart from "./components/ShoppingCart/Cart"
 import SneakerList from "./components/Sneakers/SneakerList"
+import SneakerPage from "./components/Sneakers/SneakerPage"
 import SneakerProvider from "./contexts/sneakerContext"
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <div className="nav-spacer"></div>
-      <div className="container">
-        <SneakerProvider>
+      <SneakerProvider>
+        <Navbar />
+        <div className="nav-spacer"></div>
+        <div className="container">
           <Switch>
             <Route path="/cart">
               <Cart />
             </Route>
-            <Route path="/sneakers">
+            <Route exact path="/sneakers">
               <SneakerList />
             </Route>
+            <Route path="/sneakers/:id">
+              <SneakerPage />
+            </Route>
           </Switch>
-        </SneakerProvider>
-      </div>
+        </div>
+      </SneakerProvider>
     </div>
   )
 }
