fd1f974bb574f3395c6ee79a23cb31a10d77880a
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-04-02 20:04:41 -0400

    import and wrap App in UserProvider

--- client/src/App.js
+++ client/src/App.js
@@ -6,27 +6,30 @@ import Cart from "./components/ShoppingCart/Cart"
 import SneakerList from "./components/Sneakers/SneakerList"
 import SneakerPage from "./components/Sneakers/SneakerPage"
 import SneakerProvider from "./contexts/sneakerContext"
+import UserProvider from "./contexts/userContext"
 
 function App() {
   return (
     <div className="App">
-      <SneakerProvider>
-        <Navbar />
-        <div className="nav-spacer"></div>
-        <div className="container">
-          <Switch>
-            <Route path="/cart">
-              <Cart />
-            </Route>
-            <Route exact path="/sneakers">
-              <SneakerList />
-            </Route>
-            <Route path="/sneakers/:id">
-              <SneakerPage />
-            </Route>
-          </Switch>
-        </div>
-      </SneakerProvider>
+      <UserProvider>
+        <SneakerProvider>
+          <Navbar />
+          <div className="nav-spacer"></div>
+          <div className="container">
+            <Switch>
+              <Route path="/cart">
+                <Cart />
+              </Route>
+              <Route exact path="/sneakers">
+                <SneakerList />
+              </Route>
+              <Route path="/sneakers/:id">
+                <SneakerPage />
+              </Route>
+            </Switch>
+          </div>
+        </SneakerProvider>
+      </UserProvider>
     </div>
   )
 }
