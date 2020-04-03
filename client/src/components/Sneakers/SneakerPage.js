e571c11cf93094bd7bb7060f40a291ecf2a8b4fa
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-04-02 20:07:30 -0400

    create SneakerPage, add local state and request for sneaker data by id from the url match

--- client/src/components/Sneakers/SneakerPage.js
+++ client/src/components/Sneakers/SneakerPage.js
@@ -1,25 +1,81 @@
-import React from "react"
-import { useParams, useLocation } from "react-router-dom"
+import React, { useState, useEffect, useContext } from "react"
+import axiosWithAuth from "../../utils/axiosWithAuth"
+import { userContext } from "../../contexts/userContext"
+import { useRouteMatch } from "react-router-dom"
+import "./SneakerPage.scss"
 
 export default function SneakerPage(props) {
-  const location = useLocation()
-  const { id, media, title } = location.state.sneaker
+  const [sneaker, setSneaker] = useState({
+    id: "",
+    brand: "",
+    colorway: "",
+    gender: "",
+    media: {
+      imageUrl: "",
+      smallImageUrl: "",
+      thumbUrl: ""
+    },
+    releaseDate: "",
+    retailPrice: 0,
+    styleId: "",
+    title: "",
+    year: 2020
+  })
+  const match = useRouteMatch()
+  const id = match.params.id
+  const user = useContext(userContext)
+
+  useEffect(() => {
+    async function getSneaker() {
+      try {
+        let res = await axiosWithAuth().get(`/sneakers/${id}`)
+        let sneakerData = res.data.results[0]
+        setSneaker(sneakerData)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getSneaker()
+  }, [])
+
+  function addToCart() {
+    const cart = user.cart
+    console.log(cart)
+  }
+
+  console.log(sneaker)
 
   return (
     <div className="sneaker-container">
-      <h1>{id}</h1>
-      <div className="image">
-        {media.thumbUrl ? (
-          <img src={media.thumbUrl} alt={title} />
-        ) : (
-          <h3>Coming Soon</h3>
-        )}
-      </div>
+      <div className="sneaker-grid">
+        <div className="image">
+          <img
+            src={
+              sneaker.media
+                ? sneaker.media.smallImageUrl
+                : "https://schulershoes-magento.s3.amazonaws.com/pub/media/catalog/product/placeholder/default/PLACEHOLDER.jpg"
+            }
+            alt={sneaker.title}
+          />
+        </div>
 
-      <div className="info">
-        <h5>{title}</h5>
-        <p>$200</p>
+        <div className="info">
+          <h2>{sneaker.title}</h2>
+          <div className="price">
+            <p>
+              Price:{" "}
+              <span className="light-text">
+                ${sneaker.retailPrice ? sneaker.retailPrice : 99}
+              </span>
+            </p>
+          </div>
+          <button className="cart-btn" onClick={addToCart}>
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   )
 }
+
+SneakerPage.whyDidYouRender = true
