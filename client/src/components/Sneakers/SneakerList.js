78d2998729c85f7082908de25e2160f9448dd9f5
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-04-02 20:06:53 -0400

    render SearchBar

--- client/src/components/Sneakers/SneakerList.js
+++ client/src/components/Sneakers/SneakerList.js
@@ -2,13 +2,17 @@ import React, { useContext } from "react"
 import { sneakerContext } from "../../contexts/sneakerContext"
 import SneakerCard from "./SneakerCard"
 import "./SneakerList.scss"
+import SearchBar from "../SearchBar/SearchBar"
 
 export default function SneakerList() {
-  const sneakers = useContext(sneakerContext)
+  const { sneakers } = useContext(sneakerContext)
   console.log(sneakers)
   return (
     <div className="sneaker-list">
-      <h1>Shop for Kicks</h1>
+      <div className="header">
+        <h2>Shop for Kicks</h2>
+        <SearchBar />
+      </div>
       <div className="sneaker-grid">
         {sneakers.map(sneaker => {
           return <SneakerCard key={sneaker.id} sneaker={sneaker} />
