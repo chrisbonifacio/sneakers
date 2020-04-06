6032ced199e9b3eb4163f9eb489ba1ac3844a37c
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-04-06 14:52:49 -0400

    can now add items to cart and remove them from cart

--- client/src/contexts/userContext.js
+++ client/src/contexts/userContext.js
@@ -11,8 +11,41 @@ export default function UserProvider({ children }) {
     }
   })
 
+  function addToCart(item) {
+    // make copies of prev values
+    let newItem = { ...item }
+    let prevUser = user
+    let newUser = { ...user }
+
+    // check for item in cart
+    let itemInCart = prevUser.cart.items.findIndex(cur => cur.id === item.id)
+
+    if (itemInCart !== -1) {
+      // if item is in cart, update quantity
+      newUser.cart.items[itemInCart].quantity += 1
+    } else {
+      // else, add item to list and initialize quantity to 1
+      newItem.quantity = 1
+      newUser.cart.items = prevUser.cart.items.concat(newItem)
+    }
+
+    // update total based on quantity
+    newUser.cart.total = prevUser.cart.total + item.retailPrice
+
+    setUser(newUser)
+  }
+
+  function removeFromCart(item) {
+    let prevUser = user
+    let newUser = { ...user }
+    newUser.cart.items = prevUser.cart.items.filter(cur => cur.id !== item.id)
+    newUser.cart.total = Math.min(0, prevUser.cart.total - item.retailPrice)
+
+    setUser(newUser)
+  }
+
   return (
-    <userContext.Provider value={{ user, setUser }}>
+    <userContext.Provider value={{ user, addToCart, removeFromCart }}>
       {children}
     </userContext.Provider>
   )
